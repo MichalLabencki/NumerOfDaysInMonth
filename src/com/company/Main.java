121b44330dc78e1a86a93f90b205f1f00ecb4d58
MichalLabencki <74935701+MichalLabencki@users.noreply.github.com>
2021-01-15 15:23:03 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	isLeapYear(year % 4);
+    }
+
+    public static void isLeapYear(int year) {
+        if(year >= 1 && year <= 9999) {
+        }else {
+
+
+        }
+    }
+}
